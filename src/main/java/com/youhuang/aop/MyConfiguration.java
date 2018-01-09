0a57b5ca10181230546d3055db6e399a1b137522
chenyouhuang <youhuang12@gmail.com>
2018-01-09 23:44:18 +0800

    aop

---
+++ src/main/java/com/youhuang/aop/MyConfiguration.java
@@ -0,0 +1,8 @@
+package com.youhuang.aop;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyConfiguration {
+
+}
---
+++ src/main/java/com/youhuang/aop/MyController.java
@@ -0,0 +1,30 @@
+package com.youhuang.aop;
+
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home(){
+        return "hello world!!!111111";
+    }
+    
+    @RequestMapping("/index")
+    @ResponseBody
+    String index(HttpServletRequest request){
+        Map<String, String[]> parameterMap = request.getParameterMap();
+        for (Entry<String, String[]> entry : parameterMap.entrySet()) {
+            System.out.println(entry.getKey() + "=" + entry.getValue()[0]);
+        }
+        return "index";
+    }
+}
