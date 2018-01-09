d5353763c14032720c6e4958c9d33c4bc5e67cfa
chenyouhuang <youhuang12@gmail.com>
2018-01-09 20:12:25 +0800

    mockmvc unit test

--- src/main/java/com/youhuang/demo/SampleController.java
+++ src/main/java/com/youhuang/demo/SampleController.java
@@ -1,5 +1,10 @@
 package com.youhuang.demo;
 
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,5 +16,15 @@ public class SampleController {
     @ResponseBody
     String home(){
         return "hello world!!!111111";
+    }
+    
+    @RequestMapping("/index")
+    @ResponseBody
+    String index(HttpServletRequest request){
+        Map<String, String[]> parameterMap = request.getParameterMap();
+        for (Entry<String, String[]> entry : parameterMap.entrySet()) {
+            System.out.println(entry.getKey() + "=" + entry.getValue()[0]);
+        }
+        return "index";
     }
 }
---
+++ src/test/java/com/youhuang/controller/SampleControllerTest.java
@@ -0,0 +1,42 @@
+package com.youhuang.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.youhuang.demo.DemoSpringApplication;
+import com.youhuang.demo.SampleController;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.hamcrest.Matchers;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringBootTest(classes=DemoSpringApplication.class)
+@WebAppConfiguration
+public class SampleControllerTest {
+
+    private MockMvc mvc;
+    
+    @Before
+    public void setup() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(new SampleController()).build();
+    }
+    
+    @Test
+    public void getHello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/index").param("key1", "value1").param("key2", "value2"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("indexx")));
+    }
+}
